aac68dbb62e2c8b55a42809ea7046b57891d4636
H-a-m-za <hamzamazen2002.hm@gmail.com>
2023-08-17 05:22:49 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * prrint_numbers - prints numbers 0 to 9.
+ *
+ * Return: void.
+ */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar(10);
+}
