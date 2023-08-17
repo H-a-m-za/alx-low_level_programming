30e065596a214ba4783bb257eb2506ead62ac608
H-a-m-za <hamzamazen2002.hm@gmail.com>
2023-08-17 05:40:06 -0700

    print most n

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,21 +3,21 @@
 /**
  * print_most_numbers - print all numbers cept 2 and 4.
  *
- * Return: always 0.
+ * Return: void.
  */
 
 void print_most_numbers(void)
 
 {
-	int i;
+int i;
 
-	for (i = 48; i <= 47; i++)
-	{
-		if (i == 50 || i == 52)
-		{
-			continue;
-		}
-		_putchar(i);
-	}
-	_putchar(10);
+for (i = 48; i <= 57; i++)
+{
+if ((i == 50) || (i == 52))
+{
+continue;
+}
+_putchar(i);
+}
+_putchar(10);
 }	
