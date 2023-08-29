e61792163183df4d6a57035b6c12b32aa82c092c
H-a-m-za <hamzamazen2002.hm@gmail.com>
2023-08-29 03:52:05 -0700

    ...

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,10 +10,13 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-while (*s != '\0') {
+while (*s != '\0')
+{
 char *a = accept;
-while (*a != '\0') {
-if (*s == *a) {
+while (*a != '\0')
+{
+if (*s == *a)
+{
 return (s);
 }
 a++;
